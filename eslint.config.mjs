bb2cb5d05bc1f5ec5141b01aea4fc05b855f27bd
woomin33 <woomin013@gmail.com>
2025-07-10 07:07:02 +0900

    🔧 conf: setup prettier and related esLint configuration - installed prettier, prettier-plugin-tailwindcss, and eslint-config-prettier - created prettier.config.mjs file - added 'prettier' config to eslint.config.mjs to prevent rule conflicts - added ESLint rules: prefer-const (warn), no-redeclare (warn) - added ignore paths to ESLint config: .history, dist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    rules: {
+      'prefer-const': 'warn', // 변수가 재할당 되지 않는다면 let 대신 const 사용하도록 경고
+      'no-redeclare': 1, // 변수 중복 선언시 경고
+    },
+  },
+  {
+    ignores: ['.history', 'dist'],
+  },
+  ...storybook.configs['flat/recommended'],
 ];
 
 export default eslintConfig;
